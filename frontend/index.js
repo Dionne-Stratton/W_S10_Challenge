0bb7631e55c26238cbcc961e3398f5bc8ab41630
Dionne <mylife33@hotmail.com>
2024-06-05 20:48:21 -0500

    mvp

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,18 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './state/store'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
+import App from "./components/App";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
+
+//no need to change this file
